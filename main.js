8ddd4ae8f2e9d2c3de8b35cd9f2fca2190686111
Ladaman <ladobeatles@gmail.com>
2023-08-06 18:21:04 +0400

    added a footer and messed around with some styles

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
-const logoWrapper = document.querySelector(".logo-wrapper"),
-  logoImages = logoWrapper.querySelectorAll(".logo-element");
+const logoWrapper = document.querySelector(".logo-wrapper");
+const logoImages = logoWrapper.querySelectorAll(".logo-element");
 
-const getActive = () => document.body.dataset.active === "true",
-  setActiveTo = (active) => (document.body.dataset.active = active);
+const getActive = () => {
+  document.body.dataset.active === "true";
+};
+const setActiveTo = (active) => {
+  document.body.dataset.active = active;
+};
 
 const move = (image, index, rangeX, rangeY) => {
   const active = getActive();
 
-  const translationIntensity = active ? 24 : 4,
-    maxTranslation = translationIntensity * (index + 1),
-    currentTranslation = `${maxTranslation * rangeX}% ${
-      maxTranslation * rangeY
-    }%`;
-
+  const translationIntensity = active ? 24 : 4;
+  const maxTranslation = translationIntensity * (index + 1);
+  const currentTranslation = `${maxTranslation * rangeX}% ${
+    maxTranslation * rangeY
+  }%`;
   const scale = active ? 1 + index * 0.4 : 1;
 
   image.animate(
@@ -34,12 +37,12 @@ const moveLogo = (e, logoImages) => {
 
   const rect = logoWrapper.getBoundingClientRect();
 
-  const centerX = rect.left + rect.width / 2,
-    centerY = rect.top + rect.height / 2;
+  const centerX = rect.left + rect.width / 2;
+  const centerY = rect.top + rect.height / 2;
 
-  const rangeX = (e.clientX - centerX) / radius,
-    rangeY = (e.clientY - centerY) / radius;
-  console.log(rangeX, rangeY);
+  const rangeX = (e.clientX - centerX) / radius;
+  const rangeY = (e.clientY - centerY) / radius;
+
   moveAll(logoImages, rangeX, rangeY);
 };
 
